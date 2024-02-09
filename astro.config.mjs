d5437b057622de458521168b61ab0f94cd370701
Aaron J Yoder <aaronjyoder@gmail.com>
2024-02-09 13:32:49 -0500

    Add Astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adelheid.org',
   trailingSlash: 'never',
   build: {
     format: 'file'
-  }
+  },
+  integrations: [sitemap()]
 });
