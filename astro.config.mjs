0a0a556409d9ce03eab8d0e96c493ae61b32c845
Aaron J Yoder <aaronjyoder@gmail.com>
2024-02-08 22:50:02 -0500

    Uninstall MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adelheid.org',
   trailingSlash: 'never',
   build: {
     format: 'file'
-  },
-  integrations: [mdx()]
+  }
 });
